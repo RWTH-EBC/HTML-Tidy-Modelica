8540dbc49423e69e79dd8a20fadee8620a7c6129
Peter Matthes <peter.matthes@eonerc.rwth-aachen.de>
2018-05-17 15:29:03 +0200

    Tabs to spaces in view_html_errors.py (#3)

--- view_html_errors.py
+++ view_html_errors.py
@@ -26,41 +26,41 @@ Example
 You can use this script on the command line and point it
 to your Modelica file::
 
-	$ python viewHTMLerrors.py <file> [file [...]]
+    $ python viewHTMLerrors.py <file> [file [...]]
 
 Note:
 -----
-	* This script uses Python 3.6 for printing syntax and
-	function parameter annotations.
-	* The script assumes that you have installed pytidylib
-	
-	`$ pip install pytidylib`
-	
-	* You also need to install the necessary dll's and
-	your python interpreter must be able to find the files.
-	In case of trouble just put the dll in your working dir.
-	
-	[http://binaries.html-tidy.org/](http://binaries.html-tidy.org/)
+    * This script uses Python 3.6 for printing syntax and
+    function parameter annotations.
+    * The script assumes that you have installed pytidylib
+    
+    `$ pip install pytidylib`
+    
+    * You also need to install the necessary dll's and
+    your python interpreter must be able to find the files.
+    In case of trouble just put the dll in your working dir.
+    
+    [http://binaries.html-tidy.org/](http://binaries.html-tidy.org/)
 """
 
 import argparse
 
 def getInfoRevisionsHTML(moFile):
     """Returns a list that contains the html code
-	
+    
     This function returns a list that contain the html code of the
     info and revision sections. Each element of the list
     is a string.
-	
-	Parameters
-	----------
-    moFile : str
-		The name of a Modelica source file.
     
-	Returns
-	-------
-	list 
-		The list of strings of the info and revisions section.
+    Parameters
+    ----------
+    moFile : str
+        The name of a Modelica source file.
+    
+    Returns
+    -------
+    list 
+        The list of strings of the info and revisions section.
     """
     # Open file.
     with open(moFile, mode="r", encoding="utf-8-sig") as f:
@@ -104,18 +104,18 @@ def getInfoRevisionsHTML(moFile):
 def make_string_replacements(theString : str, 
                              substitutions_dict : dict = {'\\"': '"'}) -> str:
     """Takes a string and replaces according to a given dictionary
-	
-	Parameters
-	----------
+    
+    Parameters
+    ----------
     theString : str
-		The string that contains replaceble text.
+        The string that contains replaceble text.
     substitutions_dict : dict
-		A dictionary with key:value pairs for old and new text.
-	
-	Returns
-	-------
-	str 
-		The modified string.
+        A dictionary with key:value pairs for old and new text.
+    
+    Returns
+    -------
+    str 
+        The modified string.
     """
     for k, v in substitutions_dict.items():
         theString = theString.replace(k, v)
@@ -124,21 +124,21 @@ def make_string_replacements(theString : str,
 
 def join_body(htmlList : list, substitutions_dict : dict = {'\\"': '"'}) -> str:
     """Joins a list of strings into a single string and makes replacements
-    	
-	Parameters
-	----------
+        
+    Parameters
+    ----------
     htmlList : list of str
-		The html code - each line a list entry.
+        The html code - each line a list entry.
     substitutions_dict : dict
-		A dictionary with key:value pairs for old and new text.
-		The html code is escaped in Modelica. To feed it to tidylib
-		we need to remove the escape characters.
-	
-	Returns
-	-------
-	str 
-		The html code as one string, cleared from escape characters.
-	"""    
+        A dictionary with key:value pairs for old and new text.
+        The html code is escaped in Modelica. To feed it to tidylib
+        we need to remove the escape characters.
+    
+    Returns
+    -------
+    str 
+        The html code as one string, cleared from escape characters.
+    """    
     
     body = ''.join(htmlList)  # just glue it together again
     
@@ -149,46 +149,46 @@ def join_body(htmlList : list, substitutions_dict : dict = {'\\"': '"'}) -> str:
 
 def number_print_List(htmlList : list, sep : str = '') -> None:
     """Print a list of strings with line numbers
-	
-	Should be extended by a feature to highlight a given set of line
-	numbers. This can help the reader to quickly identify the lines
-	with errors.
-	
-	Parameters
-	----------
-	htmlList : list of str
-		The html code - each line a list entry.
-	sep : str
-		String that seperates the line number from the line text.	
+    
+    Should be extended by a feature to highlight a given set of line
+    numbers. This can help the reader to quickly identify the lines
+    with errors.
+    
+    Parameters
+    ----------
+    htmlList : list of str
+        The html code - each line a list entry.
+    sep : str
+        String that seperates the line number from the line text.   
     """
     return sep.join(['{0:>5d} {1}'.format(i,line) for i,line in enumerate(htmlList)])
 
 def htmlCorrection(htmlStr : str, 
                    substitutions_dict : dict = {'"': '\\"', '<br>': '<br />',
                                                 '<br/>': '<br />'}) -> (str, str):
-	"""Returns cleaned html code and found errors
-	
-	Calls tidylib which will produce a clean version of the html code
-	and also the errors that it has found.
-	
-	Parameters
-	----------
-	htmlStr : str
-		The html code as a single string.
-	substitutions_dict : dict
-		A dictionary with key:value pairs for old and new text.
-		The html code must be escaped in Modelica. Generate properly
-		escaped code we need to add the escape characters. All the
-		while we can replace html errors that Dymola introduces.
-		i.e. '<br>' -> '<br />'
-		
-	Returns
-	-------
-	str 
-		The tidy html code with escape characters as one string.
-	str
-		The error messages from tidylib.
-	"""
+    """Returns cleaned html code and found errors
+    
+    Calls tidylib which will produce a clean version of the html code
+    and also the errors that it has found.
+    
+    Parameters
+    ----------
+    htmlStr : str
+        The html code as a single string.
+    substitutions_dict : dict
+        A dictionary with key:value pairs for old and new text.
+        The html code must be escaped in Modelica. Generate properly
+        escaped code we need to add the escape characters. All the
+        while we can replace html errors that Dymola introduces.
+        i.e. '<br>' -> '<br />'
+        
+    Returns
+    -------
+    str 
+        The tidy html code with escape characters as one string.
+    str
+        The error messages from tidylib.
+    """
     from tidylib import tidy_document
 
     # Validate the string
